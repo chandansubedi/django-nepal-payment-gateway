e95c53766a2fabdd3a0ed377eff7391ab217119e
csubedi111 <chandansubedi111@gmail.com>
2025-10-03 19:18:43 +0545

    feat: esewa payment integration

--- app/product/views.py
+++ app/product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-
+from django_esewa  import EsewaPayment
 from .models import Product, Transaction
-
+import uuid
 
 
 def index(request):
@@ -13,7 +13,30 @@ def index(request):
 
 def buy(request, id):
     product = get_object_or_404(Product, id=id)
+    uid = uuid.uuid4()
+    transaction = Transaction.objects.create(
+    product=product,
+    transaction_uuid= str(uid),
+    transaction_amount=product.price,
+    transaction_status='pending'
+
+    )
+    # Create an instance of the EsewaPayment class with the necessary parameters
+   
+    epayment = EsewaPayment(
+    product_code='EPAYTEST', 
+    success_url= f'http://localhost:8000/esewa/success/{product.id}',
+    failure_url= f'http://localhost:8000/esewa/failure/{product.id}',
+    secret_key="8gBm/:&EnhH.1/q",
+    amount=product.price,
+    total_amount=product.price,
+    transaction_uuid=transaction.transaction_uuid,
+    )
+    epayment.create_signature(transaction_uuid=transaction.transaction_uuid)
+    
     context = {
-        'product': product
+        'product': product,
+        'form': epayment.generate_form(),
+
     }
     return render(request, 'product/buy.html',context )
